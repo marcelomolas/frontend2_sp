bf49a94cacd4b6aca6c334f7dd28d8ba7cfcfa6a
elias7991 <78571779+elias7991@users.noreply.github.com>
2021-11-10 21:24:37 -0300

    listado clientes, ok

--- lib/pages/administracion.dart
+++ lib/pages/administracion.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:segundo_parcial/objects/persona.dart';
+import 'package:segundo_parcial/widgets/listaPersonas.dart';
 
-class Administracion extends StatelessWidget {
+class Administracion extends StatefulWidget {
   const Administracion({Key? key}) : super(key: key);
+
+  @override
+  _AdministracionState createState() => _AdministracionState();
+}
+
+class _AdministracionState extends State<Administracion> {
+  FetchUser _personas = FetchUser();
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +19,76 @@ class Administracion extends StatelessWidget {
         title: Text('Administración'),
         elevation: 5,
       ),
-      body: Center(
-        child: Text('Administración'),
+      body: Container(
+        child: FutureBuilder<List<Persona>>(
+            future: _personas.getUserList(),
+            builder: (context, snapshot) {
+              var data = snapshot.data;
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView.builder(
+                itemCount: data!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Row(
+                        children: [
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '${data[index].idPersona}',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            children: [
+                              Text(
+                                'Cliente: ${data[index].nombre}, ${data[index].apellido}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 16),
+                              ),
+                              Text(
+                                'Correo: ${data[index].email}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text('Teléfono: ${data[index].telefono}',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                      trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () => {},
+                        ),
+                        IconButton(
+                            icon: Icon(Icons.delete), onPressed: () => {})
+                      ]),
+                    ),
+                  );
+                },
+              );
+            }),
       ),
     );
   }
---
+++ lib/widgets/listaPersonas.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:segundo_parcial/objects/persona.dart';
+
+// url de la api
+const String url = 'equipoyosh.com';
+
+class FetchUser {
+
+
+  Future<List<Persona>> getUserList() async {
+    try {
+      var response = await http
+          .get(Uri.https(url, "/stock-nutrinatalia/persona"))
+          .timeout(const Duration(seconds: 5),
+              onTimeout: () =>
+                  throw TimeoutException("Tiempo de espera excedido"));
+      if (response.statusCode == 200) {
+        // se convierte a json
+        var data = jsonDecode(response.body) as Map<String, dynamic>;
+        //results = data.map((e) => Persona.fromMap(e)).toList();
+        var personasjson = data["lista"];
+        List<Persona> personas = [];
+        for (var personajson in personasjson) {
+          personas.add(Persona.fromMap(personajson));
+        }
+        print(data);
+        return personas;
+      } else {
+        print('error en la API ${response.statusCode}');
+      }
+    } on Exception catch (e) {
+      print('error ${e}');
+    } throw Exception('Error en la llamada a la API');
+
+    
+  }
+}
